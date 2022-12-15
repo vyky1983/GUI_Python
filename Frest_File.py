29f5750fb3e7c7b45c1f1c73f42ef4962efef6a3
vyky1983 <veronica.cernenchii@mail.ru>
2022-12-15 17:19:38 +0300

    add new inf

--- Frest_File.py
+++ Frest_File.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 
 win = tk.Tk()
+win.title(" Мое первое графичкское приложение ")
+win.geometry("500x500+1000+200")
+win.resizable(True, False)
 
 win.mainloop()
+
---
+++ My_Freist_GUI.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+win = tk.Tk()
+win.title("My frest project")
+win.geometry("500x500+200+200")
+win.minsize(200, 400)
+win.maxsize(700, 800)
+win.resizable(True, True)
+
+win.mainloop()
